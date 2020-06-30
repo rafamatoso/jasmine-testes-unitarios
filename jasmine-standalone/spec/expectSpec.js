968ee923c9bc8cebbd1254e8c9a7efcd120e8b56
Rafael Matoso <rafaelmatosog@gmail.com>
2020-06-30 20:40:12 -0300

    Definindo Verificações (Expectations)

---
+++ jasmine-standalone/spec/expectSpec.js
@@ -0,0 +1,9 @@
+describe("Expect", () => {
+  it("Deve garantir que 1 + 1 é 2", () => {
+    expect(1 + 1).toBe(2);
+  });
+
+  it("Deve garantir que 1 + 1 é 2", () => {
+    expect(1 + 1).toBe(3);
+  });
+});
