87e76fd31bcb68825fda1d4a2a0c972ab4e58c9d
Rafael Matoso <rafaelmatosog@gmail.com>
2020-06-30 21:17:31 -0300

    Comparações (Matchers): Uso do .toBe

---
+++ jasmine-standalone/spec/toBeSpec.js
@@ -0,0 +1,18 @@
+describe("Comparador toBe", () => {
+  it("Deve validar o uso do toBe", () => {
+    expect(true).toBe(true);
+    expect("Teste").toBe("Teste");
+  });
+
+  it("Deve comparar o obj1 e obj2 com toBe", () => {
+    let obj1 = { valor: true };
+    let obj2 = { valor: true };
+    expect(obj1).toBe(obj2);
+  });
+
+  it("Deve comparar se o obj1 não é igual ao obj2 com not.toBe", () => {
+    let obj1 = { valor: true };
+    let obj2 = { valor: true };
+    expect(obj1).not.toBe(obj2);
+  });
+});
