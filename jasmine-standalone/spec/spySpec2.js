3737b89a5a60bb4007230cc73fb9cb0b3f88587d
Rafael Matoso <rafaelmatosog@gmail.com>
2020-07-04 20:32:25 -0300

    and.returnValues

--- jasmine-standalone/spec/spySpec2.js
+++ jasmine-standalone/spec/spySpec2.js
@@ -10,11 +10,19 @@ describe("Testes do objeto Spy", () => {
   };
 
   beforeAll(() => {
-    spyOn(calculadora, "somar").and.returnValue(10);
+    // spyOn(calculadora, "somar").and.returnValue(10);
+    spyOn(calculadora, "somar").and.returnValues(1, 5);
     spyOn(calculadora, "subtrair");
   });
 
   it("deve retornar 10 para o método somar", () => {
-    expect(calculadora.somar(1, 2)).toEqual(10);
+    pending("desabilitado para testar outros métodos nesta suite");
+    expect(calculadora.somar(1, 2)).toEqual(1);
+  });
+
+  it("deve retornar valores distintos para cada chamada do método somar", () => {
+    expect(calculadora.somar(3, 4)).toEqual(1);
+    expect(calculadora.somar(1, 2)).toEqual(5);
+    expect(calculadora.somar(4, 4)).toBeUndefined();
   });
 });
