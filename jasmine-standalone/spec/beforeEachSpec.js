1a1c781e0f6aa1495d24cd8f91fa7bbfc3ee67d4
Rafael Matoso <rafaelmatosog@gmail.com>
2020-07-04 17:21:38 -0300

    beforeEach

---
+++ jasmine-standalone/spec/beforeEachSpec.js
@@ -0,0 +1,15 @@
+describe("Teste do beforeEach", () => {
+  let cont = 0;
+
+  beforeEach(() => {
+    cont++;
+  });
+
+  it("deve incrementar o contador para 1", () => {
+    expect(cont).toEqual(1);
+  });
+
+  it("deve icrementar o contador para 2", () => {
+    expect(cont).toEqual(2);
+  });
+});
