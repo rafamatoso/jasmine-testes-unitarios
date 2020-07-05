50256000ea15976a24dfe256c96b589f57ed8e2e
Rafael Matoso <rafaelmatosog@gmail.com>
2020-07-05 19:52:13 -0300

    Instalando e configurando o Karma com Browserify

--- jasmine-node/karma.conf.js
+++ jasmine-node/karma.conf.js
@@ -8,17 +8,17 @@ module.exports = function (config) {
 
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ["jasmine"],
+    frameworks: ["jasmine", "browserify"],
 
     // list of files / patterns to load in the browser
-    files: ["spec/**/*Spec.js"],
+    files: ["spec/**/*Spec.js", "spec/helpers/**/SpecHelper.js"],
 
     // list of files / patterns to exclude
     exclude: [],
 
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-    preprocessors: {},
+    preprocessors: { "spec/**/*Spec.js": ["browserify"] },
 
     // test results reporter to use
     // possible values: 'dots', 'progress'
--- jasmine-node/spec/exemplo/exemploSpec.js
+++ jasmine-node/spec/exemplo/exemploSpec.js
@@ -2,4 +2,8 @@ describe("Suite de teste de exemplo com NodeJS", () => {
   it("deve demonstrar o uso do Jasmine com o NodeJS", () => {
     expect(true).toEqual(true);
   });
+
+  it("deve demonstrar o uso do Karma em modo de escuta", () => {
+    expect(1).toEqual(1);
+  });
 });
