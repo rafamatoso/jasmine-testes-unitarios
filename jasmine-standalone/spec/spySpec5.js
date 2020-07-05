bc3e25e29d0f7c1183d35a3ae6558ad4eb7be204
Rafael Matoso <rafaelmatosog@gmail.com>
2020-07-05 11:36:28 -0300

    calls.reset, createSpy

---
+++ jasmine-standalone/spec/spySpec5.js
@@ -0,0 +1,14 @@
+describe("Testes do objeto jasmine.createSpy", () => {
+  let somar;
+
+  beforeAll(() => {
+    somar = jasmine.createSpy("somar");
+  });
+
+  it("deve demonstrar o uso do jasmine.createSpy", () => {
+    somar(1, 2);
+
+    expect(somar).toHaveBeenCalled();
+    expect(somar).toHaveBeenCalledWith(1, 2);
+  });
+});
